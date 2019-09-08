16b577f75bb63ee336f86af7293a78e2ec51698a
irla <irlasilva808@gmail.com>
2019-09-08 12:44:52 -0300

    Adicionar imagem na tela inicial - Menu

--- Cafeteria2/app/src/main/java/com/example/cafeteria2/CardapioActivity.java
+++ Cafeteria2/app/src/main/java/com/example/cafeteria2/CardapioActivity.java
@@ -2,13 +2,20 @@ package com.example.cafeteria2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class CardapioActivity extends AppCompatActivity {
+public class CardapioActivity extends ListActivity { //está dizendo que é uma lista
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ListView lista = getListView();
+
+        //busca as Ufs que estão declaradas na variavel Ufs - arquivo String
+        String itensMenu[] = getResources().getStringArray(R.array.itensMenu);
+
         setContentView(R.layout.activity_cardapio);
     }
 }
--- Cafeteria2/app/src/main/java/com/example/cafeteria2/MainActivity.java
+++ Cafeteria2/app/src/main/java/com/example/cafeteria2/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.cafeteria2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-    @Override
+    private ListView lista;
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lista = findViewById(R.id.menu);
+        //o this informa que a classe atual irá tratar os eventos da lista
+        lista.setOnItemClickListener(this);
+    }
+
+    public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+
+        if (position == 0) {
+            //this = origem, CardapioActivity = destino
+            Intent intent = new Intent(this, CardapioActivity.class);
+            startActivity(intent);
+        }
+        if (position == 1) {
+        }
     }
 }
