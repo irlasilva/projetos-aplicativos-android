da98d8c2e89daaafdf07fd0d172b079ed0d34d91
irla <irlasilva808@gmail.com>
2019-09-09 11:19:12 -0300

    Mostrar descrição do café selecionado - intenção da activity_descricao

--- Cafeteria2/app/src/main/java/com/example/cafeteria2/CardapioActivity.java
+++ Cafeteria2/app/src/main/java/com/example/cafeteria2/CardapioActivity.java
@@ -3,6 +3,7 @@ package com.example.cafeteria2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -23,8 +24,11 @@ public class CardapioActivity extends ListActivity { //está dizendo que é uma 
 
     //definir métodos que irá tratar os eventos dessa lista
     public void onListItemClick(ListView lista, View view, int position, long id) {
-        //converte para string o item selecionado
-        String itemLista = lista.getItemAtPosition(position).toString();
-        Toast.makeText(getApplicationContext(), itemLista, Toast.LENGTH_LONG).show();
+
+
+        Intent intent = new Intent(CardapioActivity.this,DescricaoActivity.class);
+
+        intent.putExtra("posicao", position);
+        startActivity(intent);
     }
 }
---
+++ Cafeteria2/app/src/main/java/com/example/cafeteria2/DescricaoActivity.java
@@ -0,0 +1,40 @@
+package com.example.cafeteria2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DescricaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descricao);
+
+        Intent intent = getIntent();
+        int posicao = intent.getIntExtra("posicao", 0);
+
+       // int posicao = intent.getExtras().get(Cardapio.bebidas);
+        Cardapio cafes = Cardapio.bebidas[posicao];
+
+        String tipo = cafes.getTipo();
+        TextView tipoBebida = findViewById(R.id.textDescricao);
+        tipoBebida.setText(tipo);
+
+        //preenche o nome da bebida
+        String nome = cafes.getNome();
+        TextView nomeBebida = findViewById(R.id.textNomeBebida);
+        tipoBebida.setText(nome);
+
+        //preenche a imagem do produto
+        int idImagem = cafes.getIdImagem();
+        ImageView img = findViewById(R.id.imgBebida);
+        img.setImageResource(cafes.getIdImagem());
+        img.setContentDescription(cafes.getNome()); // caso não consiga mostrar a imagem mostrará um texto
+    }
+}
+
+
