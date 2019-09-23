9e776dd85c24367c8b6b9cc9edb460230929c1d9
irla <irlasilva808@gmail.com>
2019-09-22 21:24:00 -0300

    Criar classe para editar registro - EditarBebidaActivity.java

--- Cafeteria3/app/src/main/java/com/example/cafeteria3/view/ListarBebidasActivity.java
+++ Cafeteria3/app/src/main/java/com/example/cafeteria3/view/ListarBebidasActivity.java
@@ -3,10 +3,17 @@ package com.example.cafeteria3.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.example.cafeteria3.R;
+import com.example.cafeteria3.dao.BebidaRepository;
+import com.example.cafeteria3.model.Bebida;
+
+import java.util.List;
 
 public class ListarBebidasActivity extends AppCompatActivity {
+
+    private ListView listBebidas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,4 +21,9 @@ public class ListarBebidasActivity extends AppCompatActivity {
         setContentView(R.layout.activity_listar_bebidas);
     }
 
+    protected  void getAll(){
+        BebidaRepository bebidaRepository = new BebidaRepository(this);
+        List<Bebida> bebidas = bebidaRepository.getAll();
+        //listBebidas.setAdapter(new LinhaConsultaAdapter(this, bebidas)); //linha consulta Adapter !!!!!parei aqui
+    }
 }
